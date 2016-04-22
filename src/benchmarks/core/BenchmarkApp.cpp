b0f651c0b86896b848a36c9900def03c8f61e42d
Dmitry Koplyarov <koplyarov.da@gmail.com>
2016-04-22 19:26:59 +0300

    Running benchmarks as subprocesses to force cold run

--- src/benchmarks/core/BenchmarkApp.cpp
+++ src/benchmarks/core/BenchmarkApp.cpp
@@ -12,6 +12,7 @@
 #include <benchmarks/core/ipc/MessageQueue.hpp>
 #include <benchmarks/core/utils/ThreadPriority.hpp>
 
+#include <boost/program_options.hpp>
 #include <boost/regex.hpp>
 
 
@@ -33,15 +34,36 @@ namespace benchmarks
 	{
 		try
 		{
-			if (argc <= 1)
+			using namespace boost::program_options;
+
+			std::string subtask, queue_name = "wigwagMessageQueue", benchmark;
+			int64_t num_iterations = 0;
+			std::vector<std::string> params_vec;
+
+			options_description od;
+			od.add_options()
+				("help", "Show help")
+				("subtask", value<std::string>(&subtask), "Internal option")
+				("queue", value<std::string>(&queue_name), "Internal option")
+				("iterations", value<int64_t>(&num_iterations), "Internal option")
+				("benchmark", value<std::string>(&benchmark), "Benchmark id")
+				("params", value<std::vector<std::string>>(&params_vec)->multitoken(), "Benchmark id")
+				;
+
+			positional_options_description pd;
+			pd.add("benchmark", 1).add("params", -1);
+
+			parsed_options parsed = command_line_parser(argc, argv).options(od).positional(pd).run();
+
+			variables_map vm;
+			store(parsed, vm);
+			notify(vm);
+
+			if (benchmark.empty() || vm.count("help"))
 			{
-				std::cerr << "TODO: show help" << std::endl;
+				std::cerr << boost::lexical_cast<std::string>(od) << std::endl;
 				return 0;
 			}
-
-			std::string benchmark(argv[1]);
-			if (benchmark.empty())
-				throw CmdLineException("Benchmark not specified!");
 
 			boost::regex benchmark_re(R"(([^.]+)\.([^.]+)\.([^.]+))");
 			boost::smatch m;
@@ -50,18 +72,56 @@ namespace benchmarks
 
 			boost::regex param_re(R"(([^.]+):([^.]+))");
 			std::map<std::string, SerializedParam> params;
-			for (int i = 2; i < argc; ++i)
+			for (auto&& param_str : params_vec)
 			{
-				std::string param_str(argv[i]);
 				boost::smatch m;
 				if (!boost::regex_match(param_str, m, param_re))
 					throw CmdLineException("Could not parse parameter!");
 				params[m[1]] = m[2];
 			}
 
-			SetMaxThreadPriority();
-			auto num_iterations = _suite.MeasureIterationsCount({m[1], m[2], m[3]}, params);
-			_suite.InvokeBenchmark(num_iterations, {m[1], m[2], m[3]}, params);
+			if (subtask == "measureIterationsCount")
+			{
+				MessageQueue mq(queue_name);
+				mq.SendMessage(std::make_shared<IterationsCountMessage>(_suite.MeasureIterationsCount({m[1], m[2], m[3]}, params)));
+				return 0;
+			}
+			else if (subtask == "invokeBenchmark")
+			{
+				if (vm.count("iterations") == 0)
+					throw CmdLineException("Number of iterations is not specified!");
+				MessageQueue mq(queue_name);
+				SetMaxThreadPriority();
+				_suite.InvokeBenchmark(num_iterations, {m[1], m[2], m[3]}, params);
+				return 0;
+			}
+
+			if (!subtask.empty())
+				throw CmdLineException("Unknown subtask!");
+
+			MessageQueue::Remove(queue_name);
+			MessageQueue mq(queue_name);
+
+			{
+				std::stringstream cmd;
+				cmd << argv[0] << " --subtask measureIterationsCount --queue " << queue_name << " " << benchmark;
+				for (auto&& p : params_vec)
+					cmd << " " << p;
+
+				if (system(cmd.str().c_str()) != 0)
+					throw std::runtime_error(cmd.str() + " failed!");
+			}
+
+			{
+				auto it_msg = mq.ReceiveMessage<IterationsCountMessage>();
+				std::stringstream cmd;
+				cmd << argv[0] << " --subtask invokeBenchmark --queue " << queue_name << " --iterations " << it_msg->GetCount() << " " << benchmark;
+				for (auto&& p : params_vec)
+					cmd << " " << p;
+
+				if (system(cmd.str().c_str()) != 0)
+					throw std::runtime_error(cmd.str() + " failed!");
+			}
 
 			return 0;
 		}
